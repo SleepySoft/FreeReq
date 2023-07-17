05f65128d2e5b40a9d891846a1b63e86b60b2588
Sleepy <sleepysoft@gmail.com>
2023-07-17 10:38:07 +0800

    Add test case for document key faiss.

--- plugin/KeyFaiss.py
+++ plugin/KeyFaiss.py
@@ -30,13 +30,17 @@ class KeyFaiss:
         distance, indices = self.index.search(vector, k)
         distance_key = []
         for d, i in zip(distance[0], indices[0]):
-            distance_key.append((d, self.id_to_key[i]))
+            if not np.isinf(d):
+                distance_key.append((d, self.id_to_key[i]))
         return distance_key
 
-    def add_with_keys(self, data: List[List[float]], keys: List[Any]):
+    def add_with_keys(self, data: Union[List[List[float]], np.array], keys: List[Any]):
         if len(data) != len(keys):
             raise ValueError('data and keys are not the same length.')
-        self.index.add(data)
+        embeddings = data if isinstance(data, np.ndarray) else np.array(data)
+
+        self.index.add(embeddings)
+
         for k in keys:
             self.key_to_id[k] = self.next_id
             self.id_to_key[self.next_id] = k
@@ -93,11 +97,23 @@ class DocumentKeyFaiss:
         self.ext_key_to_int_key = {}
         self.int_key_to_ext_key = {}
 
+    def search(self, xq: np.ndarray, k: int) -> List[Tuple[float, Any]]:
+        result = self.index.search(xq, k)
+
+        refactor_result = {}
+        for distance, internal_key in result:
+            key = self.int_key_to_ext_key[internal_key]
+            if key not in refactor_result or distance < refactor_result[key]:
+                refactor_result[key] = distance
+        sorted_result = sorted(refactor_result.items(), key=lambda x: x[1])
+        return [(v, k) for k, v in sorted_result]
+
     def add_document(self, data: Union[List[float], List[List[float]]], key: Any):
-        if not isinstance(data, list):
+        if not isinstance(data, np.ndarray):
             return
-        if not isinstance(data[0], list):
-            data = [data]
+        if isinstance(data, list):
+            if not isinstance(data[0], list):
+                data = [data]
 
         internal_keys = [str(uuid.uuid4().hex) for _ in data]
         self.index.add_with_keys(data, internal_keys)
@@ -117,20 +133,15 @@ class DocumentKeyFaiss:
             del self.int_key_to_ext_key[k]
         del self.ext_key_to_int_key[key]
 
-    def search(self, xq: np.ndarray, k: int) -> List[Tuple[float, Any]]:
-        result = self.index.search(xq, k)
-
-        refactor_result = {}
-        for distance, key in result:
-            if key not in refactor_result or distance < refactor_result[key]:
-                refactor_result[key] = distance
-        sorted_result = sorted(refactor_result.items(), key=lambda x: x[1])
-        return [(v, k) for k, v in sorted_result]
-
 
 # ----------------------------------------------------------------------------------------------------------------------
 
-def test_keyfaiss_add_remove_search():
+def verify_search_result_key(result: List[Tuple[float, any]], keys: List[Any], expect_index: int):
+    assert(result[0][0] == 0.0)     # 100% match
+    assert(result[0][1] == keys[expect_index])
+
+
+def test_key_faiss():
     d = 64
     raw_index = faiss.IndexFlatL2(d)
     index = KeyFaiss(raw_index)
@@ -183,11 +194,52 @@ def test_keyfaiss_add_remove_search():
     result = index.search(data[2], 1)
     assert (result[0][1] == keys[2])
 
+    print('key_faiss test finished.')
+
+
+def test_document_key_faiss():
+    d = 64  # 向量的维度
+    nb = 1000  # 文档的数量
+    ne = 3  # 每个文档的嵌入数量
+    np.random.seed(0)
+
+    # 生成数据和键
+    data = []
+    keys = []
+    for i in range(nb):
+        doc_data = np.random.random((ne, d)).astype('float32')
+        doc_key = str(i)
+        data.append(doc_data)
+        keys.append(doc_key)
+
+    index = faiss.IndexFlatL2(d)
+    key_faiss = KeyFaiss(index)
+    document_key_faiss = DocumentKeyFaiss(key_faiss)
+
+    # 将数据添加到索引中
+    for doc_data, doc_key in zip(data, keys):
+        document_key_faiss.add_document(doc_data, doc_key)
+
+    for doc_index, doc in enumerate(data):
+        for embedding in doc:
+            result = document_key_faiss.search(embedding, 1)
+            verify_search_result_key(result, keys, doc_index)
+
+    print('document_key_faiss test finished.')
+
+    # # 从索引中删除一个文档
+    # document_key_faiss.remove_document(keys[0])
+    #
+    # # 删除文档后再次搜索
+    # result = document_key_faiss.search(xq, k)
+    # print(f"删除文档后的搜索结果: {result}")
+
 
 # ---------------------------------------------------------------------------------------------------------------------
 
 def main():
-    test_keyfaiss_add_remove_search()
+    test_key_faiss()
+    test_document_key_faiss()
 
 
 # ----------------------------------------------------------------------------------------------------------------------
