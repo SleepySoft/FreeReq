9c09bfd55e80c467b80dfa4bb9992563c6b07d8a
Sleepy <yufei.qiu@se.com>
2024-01-17 13:29:20 +0800

    Remove local proxy

--- pack.bat
+++ pack.bat
@@ -1,7 +1,7 @@
 IF NOT EXIST env (
     python -m venv env
     call env\Scripts\activate
-    pip install -r requirements.txt --proxy=10.190.10.145:80
+    pip install -r requirements.txt
 ) ELSE (
     call env\Scripts\activate
 )
