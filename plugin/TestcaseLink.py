2968248f182b13f42b4fdb1f34361a1aab5f48ed
Sleepy <yufei.qiu@se.com>
2024-09-12 12:51:17 +0800

    Make test link view as dock widget.

--- plugin/TestcaseLink.py
+++ plugin/TestcaseLink.py
@@ -1,17 +1,16 @@
 import os
-import glob
 from typing import List, Dict
-
 from PyQt5.QtCore import QUrl, Qt
-from PyQt5.QtWidgets import QComboBox, QFileDialog, QAbstractItemView
-from FreeReq import IReqAgent, RequirementUI, STATIC_META_ID_PREFIX, ReqNode, IReqObserver, STATIC_FIELD_ID
-from PyQt5.QtGui import QStandardItemModel, QStandardItem, QDesktopServices
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QTableView, QMessageBox
+from PyQt5.QtGui import QStandardItemModel, QDesktopServices
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QPushButton, QTableView, \
+    QFileDialog, QAbstractItemView, QDockWidget, QHeaderView
+
+from FreeReq import IReqAgent, RequirementUI, STATIC_META_ID_PREFIX, STATIC_FIELD_ID
+from plugin.TestcaseIndexer.TestcaseFileNameScanner import TestcaseFileNameScanner
+from plugin.TestcaseIndexer.TestcaseScannerBase import TestcaseScannerBase
 
 
 # ----------------------------------------------------------------------------------------------------------------------
-from plugin.TestcaseIndexer.TestcaseFileNameScanner import TestcaseFileNameScanner
-from plugin.TestcaseIndexer.TestcaseScannerBase import TestcaseScannerBase
 
 req_agent: IReqAgent = None
 
@@ -26,31 +25,11 @@ class TestcaseSelector(QWidget):
         self.patterns = []
         self.filter = ''
         self.table_view = QTableView()
-        self.scan_path = self.main_ui.get_scan_path() if hasattr(self.main_ui, 'get_scan_path') else ''
+        self.scan_path = ''         # TODO: Load from config
         self.init_ui()
 
     def init_ui(self):
-        self.setWindowTitle("Testcase Selector")
-        self.setGeometry(0, 0, 300, 600)
-        self.setWindowFlags(self.windowFlags() | Qt.WindowStaysOnTopHint)
-
         layout = QVBoxLayout(self)
-
-        # Path input and buttons
-        path_layout = QHBoxLayout()
-        self.path_input = QLineEdit(self.scan_path)
-        self.path_input.setPlaceholderText("Scan path")
-        path_layout.addWidget(self.path_input)
-
-        self.browse_button = QPushButton("Browse...")
-        self.browse_button.clicked.connect(self.browse_path)
-        path_layout.addWidget(self.browse_button)
-
-        self.scan_button = QPushButton("Scan")
-        self.scan_button.clicked.connect(self.scan_files)
-        path_layout.addWidget(self.scan_button)
-
-        layout.addLayout(path_layout)
 
         # 初始化表格视图和模型
         self.table_model = QStandardItemModel()
@@ -73,23 +52,31 @@ class TestcaseSelector(QWidget):
         # 将表格视图添加到布局中
         layout.addWidget(self.table_view)
 
-        # Load the scan path
-        self.load_scan_path()
+        # Path input and buttons
+        path_layout = QHBoxLayout()
+        self.path_input = QLineEdit(self.scan_path)
+        self.path_input.setPlaceholderText("Scan path")
+        path_layout.addWidget(self.path_input)
 
-    def show_right_upper(self):
-        main_ui_geometry = self.main_ui.frameGeometry()
-        self.move(main_ui_geometry.topRight() - self.rect().topRight())
-        self.show()
+        self.browse_button = QPushButton("Browse...")
+        self.browse_button.clicked.connect(self.browse_path)
+        path_layout.addWidget(self.browse_button)
+
+        self.scan_button = QPushButton("Scan")
+        self.scan_button.clicked.connect(self.scan_files)
+        path_layout.addWidget(self.scan_button)
+
+        layout.addLayout(path_layout)
 
     def set_req_id_filter(self, req_filter: str):
         self.filter = req_filter
         self.update_testcase_index()
 
-    def load_scan_path(self):
-        # Load the scan path from the main UI or a config file
+    def set_scan_path(self, scan_path: str):
+        self.scan_path = scan_path
         self.path_input.setText(self.scan_path)
 
-    def update_scan_pattern(self, patterns: List[str]):
+    def set_scan_pattern(self, patterns: List[str]):
         self.patterns = patterns
 
     def browse_path(self):
@@ -123,36 +110,45 @@ class TestcaseSelector(QWidget):
             self.table_model.setData(self.table_model.index(row, 0), file_name)
             self.table_model.setData(self.table_model.index(row, 1), file_path)
 
+        # 自动调整列宽度
+        self.table_view.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+
     def open_file(self, index):
         file_path = self.table_model.data(self.table_model.index(index.row(), 1))
         if file_path:
             QDesktopServices.openUrl(QUrl.fromLocalFile(file_path))
 
-    def closeEvent(self, event):
-        # Hide the window instead of closing it
-        self.hide()
-        event.ignore()
 
-
-test_case_selector: TestcaseSelector = None
+test_case_viewer: TestcaseSelector = None
+dock_test_case_viewer: QDockWidget = None
 
 
 def on_hook_req_loaded(_):
     meta_config = req_agent.get_req_meta()
+
     req_id_prefix = meta_config.get(STATIC_META_ID_PREFIX)
     req_id_pattern = [prefix + r'\d{5}' for prefix in req_id_prefix]
-    test_case_selector.update_scan_pattern(req_id_pattern)
-    test_case_selector.scan_files()
+    test_case_viewer.set_scan_pattern(req_id_pattern)
+
+    guess_test_case_path = os.path.join(req_agent.get_req_path(), 'Testcase')
+    if os.path.exists(guess_test_case_path):
+        test_case_viewer.set_scan_path(guess_test_case_path)
+
+    test_case_viewer.scan_files()
 
 
 def on_hook_requirement_tree_selection_changed(_, __):
-    _, req_node = test_case_selector.main_ui.get_selected()
+    _, req_node = test_case_viewer.main_ui.get_selected()
     if req_node is not None:
-        test_case_selector.set_req_id_filter(req_node.get(STATIC_FIELD_ID, ''))
+        test_case_viewer.set_req_id_filter(req_node.get(STATIC_FIELD_ID, ''))
 
 
 def on_test_case_selector_button_click():
-    test_case_selector.show_right_upper()
+    if dock_test_case_viewer.isVisible():
+        dock_test_case_viewer.hide()
+    else:
+        dock_test_case_viewer.show()
+    # test_case_viewer.show_right_upper()
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -178,8 +174,15 @@ def req_agent_prepared(req: IReqAgent):
 
 
 def after_ui_created(req_ui: RequirementUI):
-    global test_case_selector
-    test_case_selector = TestcaseSelector(req_ui, TestcaseFileNameScanner('', []))
+    global test_case_viewer
+    global dock_test_case_viewer
+
+    dock_test_case_viewer = QDockWidget("Testcase Link", req_ui)
+    test_case_viewer = TestcaseSelector(req_ui, TestcaseFileNameScanner('', []))
+
+    dock_test_case_viewer.hide()
+    dock_test_case_viewer.setWidget(test_case_viewer)
+    req_ui.addDockWidget(Qt.RightDockWidgetArea, dock_test_case_viewer)
 
     req_ui.on_req_loaded.add_post_hook(on_hook_req_loaded)
     req_ui.on_requirement_tree_selection_changed.add_post_hook(on_hook_requirement_tree_selection_changed)
@@ -187,7 +190,3 @@ def after_ui_created(req_ui: RequirementUI):
     test_case_selector_button = QPushButton('Testcase Link')
     test_case_selector_button.clicked.connect(on_test_case_selector_button_click)
     req_ui.edit_board.layout_plugin_area.addWidget(test_case_selector_button)
-
-
-
-
